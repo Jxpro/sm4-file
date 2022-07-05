276da1c04b88ad59aeecc2981f7dc8a9efc2f411
jokerxin <jxpro@qq.com>
2022-07-06 01:14:57 +0800

    Add some useful methods

--- sm4_file.py
+++ sm4_file.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-
+import json
 import os.path
 import re
 import time
@@ -11,6 +11,12 @@ from crypto.sm4 import *
 
 class SM4File:
     def __init__(self):
+        self.config = {}
+        # 读取配置
+        if os.path.exists('config.json'):
+            with open('config.json', 'r', encoding='utf-8') as f:
+                self.config = json.load(f)
+
         # 调用Tk()创建主窗口
         self.window = tk.Tk()
         # 设置窗口居中
@@ -42,8 +48,13 @@ class SM4File:
         self.progress_string = tk.StringVar()
 
         # 设置提示文本
-        self.file_path.set("请选择待加密文件")
-        self.dic_path.set("请选择输出路径")
+        self.file_path.set(self.config.get('file_path', '请选择待加密文件'))
+        self.dic_path.set(self.config.get('dic_path', '请选择输出路径'))
+        self.key_string.set(self.config.get('key', ''))
+        self.iv_string.set(self.config.get('iv', ''))
+        self.nonce_string.set(self.config.get('nonce', ''))
+
+        # 设置进度条
         self.progress_string.set("")
         self.total_size_str = ""
         self.now_size_str = ""
@@ -55,6 +66,11 @@ class SM4File:
         # 绘制路径选择按钮
         tk.Button(window, text='选择文件', command=self.get_file_path).grid(row=0, column=4, padx=10, pady=5)
         tk.Button(window, text='选择路径', command=self.get_dic_path).grid(row=1, column=4, padx=10, pady=5)
+
+        # 绘制随机生成按钮
+        tk.Button(window, text='随机生成', command=lambda: self.random_num(0)).grid(row=2, column=4, padx=10, pady=5)
+        tk.Button(window, text='随机生成', command=lambda: self.random_num(1)).grid(row=3, column=4, padx=10, pady=5)
+        tk.Button(window, text='随机生成', command=lambda: self.random_num(2)).grid(row=4, column=4, padx=10, pady=5)
 
         # 绘制密钥文本框
         tk.Entry(window, text=self.key_string, width=42).grid(row=2, column=1, columnspan=3)
@@ -80,8 +96,21 @@ class SM4File:
         self.progress_bar.grid(row=7, column=1, columnspan=3, pady=20)
         tk.Label(window, textvariable=self.progress_string, width=12, height=2).grid(row=7, column=4)
 
+        # 计算文件大小
+        if self.config.get('file_path'):
+            self.total_size_str = self.compute_size_str(os.path.getsize(str(self.config.get('file_path'))))
+            self.progress_string.set("0/" + self.total_size_str)
+            self.progress_bar['value'] = 0
+            self.window.update()
+
         # 调用mainloop()显示主窗口
         window.mainloop()
+
+        # 结束时更新配置
+        with open('config.json', 'w', encoding='utf-8') as f:
+            # ensure_ascii 参数保证中文不会变为Unicode编码
+            # indent 参数表示缩减4个空格，保证不会将结果输出为一行
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
 
     def window_center(self, width, height):
         """
@@ -109,8 +138,13 @@ class SM4File:
 
         # 返回文件路径并获取大小,并将输出目录默认为该目录
         if file_path:
+            # 设置路径
             self.file_path.set(file_path)
             self.dic_path.set(os.path.dirname(file_path))
+            self.config.update({'file_path': file_path})
+            self.config.update({'dic_path': os.path.dirname(file_path)})
+
+            # 计算文件大小
             self.total_size_str = self.compute_size_str(os.path.getsize(file_path))
             self.progress_string.set("0/" + self.total_size_str)
             self.progress_bar['value'] = 0
@@ -127,6 +161,21 @@ class SM4File:
         # 返回文件夹路径
         if dic_path:
             self.dic_path.set(dic_path)
+            self.config.update({'dic_path': dic_path})
+
+    def random_num(self, i):
+        """
+        产生并设置随机数
+        :param i: 0-key，1-iv，2-nonce
+        :return: None
+        """
+        random_str = os.urandom(16).hex()
+        if i == 0:
+            self.key_string.set(random_str)
+        elif i == 1:
+            self.iv_string.set(random_str)
+        else:
+            self.nonce_string.set(random_str)
 
     def crypt(self, mode, is_encrypt):
         """
@@ -146,6 +195,13 @@ class SM4File:
         if mode != SM4_ECB_MODE:
             assert len(self.iv_string.get()) == 32, messagebox.showinfo('警告', '初始向量长度需为16字节（32位十六进制数）')
             assert re.match(r'^[0-9a-fA-F]{32}$', self.iv_string.get()), messagebox.showinfo('提示', 'iv需要为十六进制数')
+
+        # 更新配置
+        self.config.update({'key': self.key_string.get()})
+        if self.iv_string != '':
+            self.config.update({'iv': self.iv_string.get()})
+        if self.nonce_string != '':
+            self.config.update({'nonce': self.nonce_string.get()})
 
         # 检查文件是否存在
         if not os.path.exists(self.file_path.get()):
@@ -177,30 +233,35 @@ class SM4File:
         # 计时开始
         start_time = time.time()
 
-        # 加密/解密
-        if is_encrypt:
-            while True:
-                data = f_in.read(10240)
-                if not data:
-                    break
-                # 尝试使用多线程，但是效果不佳
-                # _thread.start_new_thread(lambda d: f_out.write(sm4_suite.encrypt(d)), (data,))
-                f_out.write(sm4_suite.encrypt(data))
-                self.progress_bar['value'] += len(data)
-                self.progress_string.set(
-                    self.compute_size_str(self.progress_bar['value']) + "/" + self.total_size_str)
-                self.window.update()
-        else:
-            while True:
-                # 16位是可能的额外填充字节（12400+16）
-                data = f_in.read(10256)
-                if not data:
-                    break
-                f_out.write(sm4_suite.decrypt(data))
-                self.progress_bar['value'] += len(data)
-                self.progress_string.set(
-                    self.compute_size_str(self.progress_bar['value']) + "/" + self.total_size_str)
-                self.window.update()
+        # 捕获加解密错误
+        try:
+            # 加密/解密
+            if is_encrypt:
+                while True:
+                    data = f_in.read(10240)
+                    if not data:
+                        break
+                    # 尝试使用多线程，但是效果不佳
+                    # _thread.start_new_thread(lambda d: f_out.write(sm4_suite.encrypt(d)), (data,))
+                    f_out.write(sm4_suite.encrypt(data))
+                    self.progress_bar['value'] += len(data)
+                    self.progress_string.set(
+                        self.compute_size_str(self.progress_bar['value']) + "/" + self.total_size_str)
+                    self.window.update()
+            else:
+                while True:
+                    # 16位是可能的额外填充字节（12400+16）
+                    data = f_in.read(10256)
+                    if not data:
+                        break
+                    f_out.write(sm4_suite.decrypt(data))
+                    self.progress_bar['value'] += len(data)
+                    self.progress_string.set(
+                        self.compute_size_str(self.progress_bar['value']) + "/" + self.total_size_str)
+                    self.window.update()
+        except:
+            messagebox.showinfo('错误', '加密错误' if is_encrypt else '解密错误')
+            return
 
         # 计时结束
         end_time = time.time()
